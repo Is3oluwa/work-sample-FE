615763e10221f49b5a3546abcfaac2a27656735b
Iseoluwa <iseoyedotun@gmail.com>
2023-04-06 07:48:49 +0100

    removed cors header

--- src/api/index.js
+++ src/api/index.js
@@ -1,24 +1,22 @@
-import axios from "axios";
+import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: "https://work-sample-be.onrender.com",
+  baseURL: 'https://work-sample-be.onrender.com',
   headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin":
-      "https://642dda3a4ae7d42755fe75d3--work-sample-ecommerce.netlify.app",
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type, Authorization",
-  },
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+  }
 });
 
-apiClient.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token"); // assuming the token is stored in local storage
+apiClient.interceptors.request.use(config => {
+  const token = localStorage.getItem('token'); // assuming the token is stored in local storage
   if (token) {
-    console.log("token dey");
+    console.log("token dey")
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
 
 export default apiClient;
+
